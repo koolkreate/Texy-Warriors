bfc27a17daadccbdcba70aedcf773a896896122d
dakinfemiwa <daveak1602@gmail.com>
2025-12-29 13:55:51 +0000

    Commenting for forms.py

--- src/forms.py
+++ src/forms.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, validators
 
+"""
+This file handles all forms - login forms and sign up forms for now
+"""
+
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[validators.DataRequired()])
     password = PasswordField("Password")
